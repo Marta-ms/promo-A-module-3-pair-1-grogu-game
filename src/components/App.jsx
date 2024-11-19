075d3783b2d55ed034ef405741f2568bfd0c5cdb
Marta-ms <Marta-ms>
2024-11-19 10:17:05 +0100

    save

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './components/App.jsx'
+//import { useState } from 'react'
+
 
 import "../scss/App.scss";
 //import groguImg from "../images/grogu.jpg";
 
 function App() {
+//const [randomNumber, setRandomNumber] = useState ("");
+
   return (
     <>
     <header>
@@ -52,5 +52,6 @@ function App() {
 
   )
 }
+
 
 export default App
