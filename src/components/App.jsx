632f396b02c792f95a342a62657afc51085a9d60
Marta-ms <Marta-ms>
2024-11-19 11:29:29 +0100

    random number OK

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,30 @@
 //import { useState } from 'react'
-
-
+import Header from './Header';
+import Board from "./Board";
 import "../scss/App.scss";
+import {useState} from "react";
 //import groguImg from "../images/grogu.jpg";
 
 function App() {
-//const [randomNumber, setRandomNumber] = useState ("");
+  const [randomNumber, setRandomNumber] = useState ("Lanzar dado");
+  const rollDice = () => {
+    const randomNumber = Math.floor(Math.random() *4) +1; //genera un número aleatorio entre 1 y 4
+    setRandomNumber(randomNumber);
+    //console.log(randomNumber);
 
+
+  };
   return (
     <>
-    <header>
-      <h1>¡Cuidado con Grogu!</h1>
-    </header>
+    
     <main className="page">
-      <section className="board">
-        <div className="cell"><div className="grogu">👣</div></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-        <div className="cell"></div>
-      </section>
+      
+      <Header />
+      <Board />
+      
   
       <section>
-        <button className="dice">Lanzar Dado</button>
+        <button className="dice" onClick={rollDice} >{randomNumber}</button>
         <div className="game-status">En curso</div>
       </section>
 
@@ -51,7 +51,33 @@ function App() {
     </>
 
   )
+
+  
+
+
+
+
+
 }
+
+//Pasos 
+/* Cuando la usuaria haga click en lanzar dado
+ - generar número aleatorio entre 1 y 4
+ - si es 4 --> grogu avanza 1 casilla 
+      y si  es 1 -->quitar una galleta
+      si es 2 --> quitar una huevo
+      si es 3 --> quitar una rana
+- Saber en qué posición se encuentra el grogu o mercancias 
+   - Mostrar en pantalla el estado del juego
+      -si grogu avanza --> mostrar mesanje  "Grogu ha avanzado"
+      -si se quita una galleta --> mostrar mensaje "Se ha quitado una galleta"
+      -si se quita un huevo --> mostrar mensaje "Se ha quitado un huevo"
+      -si se quita una rana --> mostrar mensaje "Se ha quitado una rana"
+
+
+      */
+
+
 
 
 export default App
